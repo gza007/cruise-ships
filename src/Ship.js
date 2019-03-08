af7fa510edacfca64eee2b62f219e212ee852432
Geevan Patel <geevan.peter.patel@gmail.com>
2019-03-08 23:07:08 +0000

    added comments to Port and Test Port files

--- src/Ship.js
+++ src/Ship.js
@@ -2,6 +2,8 @@ function Ship(currentPort) {
   this.currentPort = currentPort;
 };
 
+// Above property(?) began as startingPort, now changed. 
+
 Ship.prototype.setSail = function () {
   this.currentPort = null;
 };
