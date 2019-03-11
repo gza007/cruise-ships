d2e36e7c05bbff56815a915584a23d0b6d9721c7
Geevan Patel <geevan.peter.patel@gmail.com>
2019-03-11 00:12:42 +0000

    Added addShip and removeShip Tests and methods to Port.test and Port.js files

--- __tests__/Port.test.js
+++ __tests__/Port.test.js
@@ -5,8 +5,44 @@ describe('Port', () => {
   it('can be instantiated', () => {
     expect(new Port()).toBeInstanceOf(Object);
   });
-  const port = new Port('Port of Spain')
+  const port = new Port('Port of Spain');
   it('has a name', () => {
     expect(port.portName).toBe('Port of Spain');
+  });
+});
+
+describe('add ship', () => {
+  it('adds ship to the list of ships at the Port', () => {
+    const port = new Port('Dover');
+    const ship = {};
+    port.addShip(ship);
+    expect(port.ships).toContain(ship);
+  });
+});
+
+describe('remove ship', () => {
+  it('removes ship from the list of ships at the Port', () => {
+    const port = new Port('Dover');
+    const titanic = {};
+    const queenMary = {};
+    port.addShip(titanic);
+    port.addShip(queenMary);
+    port.removeShip(queenMary);
+    expect(port.ships).toEqual([titanic]);
+  });
+});
+
+// Extra Test below, to see if an undefined ship being passed will alter the array
+
+describe('remove ship', () => {
+  it('removes ship from the list of ships at the Port', () => {
+    const port = new Port('Dover');
+    const titanic = {};
+    const queenMary = {};
+    const blackPearl = {};
+    port.addShip(titanic);
+    port.addShip(queenMary);
+    port.removeShip(blackPearl);
+    expect(port.ships).toEqual([titanic, queenMary]);
   });
 });
--- src/Port.js
+++ src/Port.js
@@ -1,5 +1,20 @@
 function Port(portName) {
   this.portName = portName;
+  this.ships = [];
 }
+
+Port.prototype.addShip = function (ship) {
+  this.ships.push(ship);
+};
+
+Port.prototype.removeShip = function (ship) {
+  this.ships = this.ships.filter(x => x !== ship)
+};
+
+/* THIS METHOD REMOVES A SHIP FROM PORT LIST EVEN IF AN UNDEFINED SHIP IS PASSED
+Port.prototype.removeShip = function removeShip(ship) {
+  this.ships.splice(this.ships.indexOf[ship], 1);
+};
+*/
 
 module.exports = Port;
