a1bdc03b7143f0c1ce629a0ae592ac2fbd4ca107
Geevan Patel <geevan.peter.patel@gmail.com>
2019-03-21 23:00:20 +0000

    made both test suites DRY

--- __tests__/Port.test.js
+++ __tests__/Port.test.js
@@ -1,64 +1,50 @@
 /* globals describe it expect */
 const Port = require('../src/Port.js');
 
+let port;
+let titanic;
+let queenMary;
+let blackPearl;
+
+beforeEach(() => {
+  port = new Port('Dover');
+  titanic = {};
+  queenMary = {};
+  blackPearl = {};
+});
+
 describe('Port', () => {
   it('can be instantiated', () => {
-    expect(new Port()).toBeInstanceOf(Object);
+    expect(port).toBeInstanceOf(Object);
   });
-  const port = new Port('Port of Spain');
   it('has a name', () => {
-    expect(port.portName).toBe('Port of Spain');
+    expect(port.portName).toBe('Dover');
   });
 });
 
 describe('add ship', () => {
   it('adds ship to the list of ships at the Port', () => {
-    const port = new Port('Dover');
-    const ship = {};
-    port.addShip(ship);
-    expect(port.ships).toContain(ship);
-  });
-});
-
-describe('remove ship', () => {
-  it('removes ship from the list of ships at the Port', () => {
-    const port = new Port('Dover');
-    const titanic = {};
-    const queenMary = {};
     port.addShip(titanic);
-    port.addShip(queenMary);
-    port.removeShip(queenMary);
     expect(port.ships).toEqual([titanic]);
   });
 });
 
-// Extra Test below, checks if an "undefined" ship passed in removeShip method alters the array.
-// Note: The "False Positive" method in ship.js file fails this test
-
 describe('remove ship', () => {
-  it('removes ship from the list of ships at the Port', () => {
-    const port = new Port('Dover');
-    const titanic = {};
-    const queenMary = {};
-    const blackPearl = {};
-    port.addShip(titanic);
-    port.addShip(queenMary);
+  it('removes last element from the array', () => {
+    port.ships = [titanic, queenMary];
+    port.removeShip(queenMary);
+    expect(port.ships).toEqual([titanic]);
+    expect(port.ships).not.toContain(queenMary);
+  });
+  it('checks array is unaffected by calling removeShip method with ship not already in array', () => {
+    port.ships = [titanic, queenMary];
     port.removeShip(blackPearl);
     expect(port.ships).toEqual([titanic, queenMary]);
   });
-});
-
-// Extra test below, to see if the 2nd ship in the array can be removed (Whitney's test)
-
-describe('Port Constructor', () => {
-  const port = new Port('Dublin');
-  const mockShip = jest.fn();
-  const mockShip2 = jest.fn();
-  it('removeShip method', () => {
-    port.ships = [mockShip2, mockShip];
-    port.removeShip(mockShip);
-    expect(port.ships).toEqual([mockShip2]);
-    expect(port.ships).not.toContain(mockShip);
+  it('removes first in the array', () => {
+    port.ships = [queenMary, titanic];
+    port.removeShip(queenMary);
+    expect(port.ships).toEqual([titanic]);
+    expect(port.ships).not.toContain(queenMary);
   });
 });
-
--- __tests__/Ship.test.js
+++ __tests__/Ship.test.js
@@ -3,41 +3,37 @@ const Ship = require('../src/Ship.js');
 const Port = require('../src/Port.js');
 const Itinerary = require('../src/Itinerary.js');
 
+let ship;
+let dover;
+let calais;
+let itinerary;
+
+beforeEach(() => {
+  dover = new Port('Dover');
+  calais = new Port('Calais');
+  itinerary = new Itinerary([dover, calais]);
+  ship = new Ship(itinerary);
+});
+
 describe('Ship', () => {
-  describe('with ports and an itinerary', () => {
-    let ship;
-    let dover;
-    let calais;
-    let itinerary;
-    beforeEach(() => {
-      dover = new Port('Dover');
-      calais = new Port('Calais');
-      itinerary = new Itinerary([dover, calais]);
-      ship = new Ship(itinerary);
-    });
-    it('can be instantiated', () => {
-      expect(ship).toBeInstanceOf(Object);
-    });
-    it('has a starting port', () => {
-      expect(ship.currentPort).toBe(dover);
-    });
-    it('can set sail', () => {
-      ship.setSail();
-      expect(ship.currentPort).toBeFalsy();
-      expect(dover.ships).not.toContain(ship);
-    });
-    it('gets added to port on instantiation', () => {
-      expect(dover.ships).toContain(ship);
-    });
+  it('can be instantiated', () => {
+    expect(ship).toBeInstanceOf(Object);
+  });
+  it('has a starting port', () => {
+    expect(ship.currentPort).toBe(dover);
+  });
+  it('can set sail', () => {
+    ship.setSail();
+    expect(ship.currentPort).toBeFalsy();
+    expect(dover.ships).not.toContain(ship);
+  });
+  it('gets added to port on instantiation', () => {
+    expect(dover.ships).toContain(ship);
   });
 });
 
 describe('Dock', () => {
   it('can dock at a different port', () => {
-    const dover = new Port('Dover');
-    const calais = new Port('Calais');
-    const itinerary = new Itinerary([dover, calais]);
-    const ship = new Ship(itinerary);
     ship.setSail();
     ship.dock();
     expect(ship.currentPort).toBe(calais);
